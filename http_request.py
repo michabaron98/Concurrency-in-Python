4e4d3305295da3b500c3357e5e54cc2387b3e6f3
MichalBaron <MBaron@trueblue.com>
2024-03-16 12:08:27 +0100

    aiohttp

--- http_request.py
+++ http_request.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def get_data(url: str, endpoint: dict,headers = {
+def get_data(url: str, endpoint: str, headers = {
             "Content-Type": "application/json"}):
 	response = requests.get(url=url+endpoint, headers=headers)
 	return response.json()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from common import *
-from async_http_request import async_get_data
-from http_request import get_data
 import asyncio
+import aiohttp
+
+from common import *
+from async_http_request import async_get_data, async_get_data_aiohttp
+from http_request import get_data
 from timer import calculate_execution_time, calculate_async_execution_time
 
 
@@ -20,8 +22,16 @@ def main() -> None:
 			data = get_data(API_URL, Endpoints.DINOSAURS_RANDOM)
 			print(data['Name'])
 
-	max_range=10
+	@calculate_async_execution_time
+	async def async_run_aiohttp():
+		async with aiohttp.ClientSession() as session:
+			tasks = [asyncio.ensure_future(async_get_data_aiohttp(API_URL, Endpoints.DINOSAURS_RANDOM, session)) for i in range(0, max_range+1)]
+			for data in await asyncio.gather(*tasks):
+				print(data['Name'])
+
+	max_range=100
 	asyncio.run(async_run())
+	asyncio.run(async_run_aiohttp())
 	run()
 
 main()
